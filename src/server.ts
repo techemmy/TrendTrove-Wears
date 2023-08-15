4d7cb443e0179b1acc857dc8276fbe49bd736817
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-08-15 20:08:08 +0100

    feat: configure ejs with express and add new routes for frontend pages

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,37 @@ import express from "express";
 const app: Application = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-  res.send("ok");
+  res.render('index')
 });
+
+app.get("/about", (req, res) => {
+  res.render('about')
+})
+
+app.get("/shop", (req, res) => {
+  res.render('shop')
+})
+
+app.get("/single-product", (req, res) => {
+  res.render('single-product')
+})
+
+app.get("/cart", (req, res) => {
+  res.render('cart')
+})
+
+app.get("/checkout", (req, res) => {
+  res.render('checkout')
+})
+
+app.get("/thankyou", (req, res) => {
+  res.render('thankyou')
+})
 
 app.listen(PORT, () => {
   console.log(`🔥[Server] listening on port ${PORT}`);
