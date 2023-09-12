80fd8a1d5f745e74b0d9c041589439cc6ddbff14
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-09-12 21:51:29 +0100

    chore: add type for Product Model

---
+++ src/types/models/productTypes.ts
@@ -0,0 +1,12 @@
+export interface ProductAttributes {
+  id?: number;
+  name: string;
+  price: string;
+  category: string;
+  size?: string;
+  sizes?: string[];
+  color?: string;
+  shortDescription: string;
+  longDescription?: string;
+  available: boolean;
+}
