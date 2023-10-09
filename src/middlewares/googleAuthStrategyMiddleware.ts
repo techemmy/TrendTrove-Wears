65f476c1ac3dfb6b22aff87c8ae6fecc64c1f133
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-10-09 12:17:46 +0100

    feat: add strategy for authentication & authorization with Google

---
+++ src/middlewares/googleAuthStrategyMiddleware.ts
@@ -0,0 +1,31 @@
+import passport from 'passport';
+import { Strategy } from 'passport-google-oauth2';
+import { User } from '../models';
+import { googleOAuthConfig } from '../config';
+
+export default (): void => {
+    passport.use(
+        new Strategy(
+            {
+                clientID: googleOAuthConfig.CLIENT_ID,
+                clientSecret: googleOAuthConfig.CLIENT_SECRET,
+                callbackURL: googleOAuthConfig.CALLBACK_URL,
+                passReqToCallback: true,
+            },
+            async function (request, accessToken, refreshToken, profile, done) {
+                try {
+                    const [user] = await User.findOrCreate({
+                        where: { email: profile.email },
+                        defaults: {
+                            email: profile.email,
+                            providerIdentity: profile.provider,
+                        },
+                    });
+                    return done(null, user);
+                } catch (error) {
+                    return done(error, profile);
+                }
+            }
+        )
+    );
+};
