16b57ac194440245ef60072f90dcced46e28a05a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-10-09 12:21:53 +0100

    feat: develop auth route

---
+++ src/routes/authRoute.ts
@@ -0,0 +1,30 @@
+import router, { type Router } from 'express';
+import passport from 'passport';
+import googleAuthStrategyMiddleware from '../middlewares/googleAuthStrategyMiddleware';
+
+const authRouter: Router = router();
+
+googleAuthStrategyMiddleware();
+
+authRouter.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+authRouter.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+authRouter.get(
+    '/google',
+    passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+authRouter.get(
+    '/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/shop',
+        failureRedirect: '/auth/signup',
+    })
+);
+
+export default authRouter;
