090f3e1259a547f2155c9d9d01c348465da9501a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-10-12 22:45:09 +0100

    fix: add empty string as default value for session secret config if the app fails fails to retrieve a secret from the environment, the empty string will cause the app to throw an error

--- src/config/sessionConfig.ts
+++ src/config/sessionConfig.ts
@@ -1,7 +1,7 @@
 const ONE_WEEK = 7 * 24 * 60 * 60 * 1000;
 
 export default {
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET ?? '',
     resave: false,
     saveUninitialized: false,
     cookie: { secure: false, maxAge: ONE_WEEK },
