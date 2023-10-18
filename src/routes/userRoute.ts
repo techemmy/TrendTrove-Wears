6e16de12f61b6b67e1ffc134a054e4375efb046c
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-10-18 13:46:14 +0100

    feat(style): show user's profile image, name, email and phoneNumber on the frontend

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import type { IRequestWithAuthenticatedUser } from '../types/requestTypes';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-    res.render('user/user-profile');
+userRouter.get('/', (req: IRequestWithAuthenticatedUser, res) => {
+    res.render('user/user-profile', { user: req.user });
 });
 
 export default userRouter;
