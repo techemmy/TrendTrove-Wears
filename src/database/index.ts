9c090c2d0ea8365e84eb49f8446e180144890fdc
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-10-18 15:09:47 +0100

    refactor: update associations to use db object

--- src/database/index.ts
+++ src/database/index.ts
@@ -42,39 +42,39 @@ const db = {
     users: userFactory(sequelize),
     roles: roleFactory(sequelize),
     products: productFactory(sequelize),
-    address: addressFactory(sequelize),
-    cartItem: cartItemFactory(sequelize),
-    cart: cartFactory(sequelize),
-    coupon: couponFactory(sequelize),
+    addresses: addressFactory(sequelize),
+    cartItems: cartItemFactory(sequelize),
+    carts: cartFactory(sequelize),
+    coupons: couponFactory(sequelize),
 };
 
 // MODEL RELATIONSHIPS
 // User <=> Role
-User.belongsToMany(Role, { through: 'userRoles' });
-Role.belongsToMany(User, { through: 'userRoles' });
+db.users.belongsToMany(db.roles, { through: 'userRoles' });
+db.roles.belongsToMany(db.users, { through: 'userRoles' });
 
 // User <=> Product
-User.hasMany(Product, { foreignKey: 'userId' });
-Product.belongsTo(User, { foreignKey: 'userId' });
+db.users.hasMany(db.products, { foreignKey: 'userId' });
+db.products.belongsTo(db.users, { foreignKey: 'userId' });
 
 // User <=> Cart
-User.hasMany(Cart, { foreignKey: 'userId' });
-Cart.belongsTo(User, { foreignKey: 'userId' });
+db.users.hasMany(db.carts, { foreignKey: 'userId' });
+db.carts.belongsTo(db.users, { foreignKey: 'userId' });
 
 // User <=> Address
-User.hasOne(Address, { foreignKey: 'userId' });
-Address.belongsTo(User, { foreignKey: 'userId' });
+db.users.hasOne(db.addresses, { foreignKey: 'userId' });
+db.addresses.belongsTo(db.users, { foreignKey: 'userId' });
 
 // Product <=> CartItem
-Product.hasMany(CartItem, { foreignKey: 'productId' });
-CartItem.belongsTo(Product, { foreignKey: 'productId' });
+db.products.hasMany(db.cartItems, { foreignKey: 'productId' });
+db.cartItems.belongsTo(db.products, { foreignKey: 'productId' });
 
 // Cart <=> CartItem
-Cart.hasMany(CartItem, { foreignKey: 'cartId' });
-CartItem.belongsTo(Cart, { foreignKey: 'cartId' });
+db.carts.hasMany(db.cartItems, { foreignKey: 'cartId' });
+db.cartItems.belongsTo(db.carts, { foreignKey: 'cartId' });
 
 // Coupon <=> Cart
-Coupon.hasMany(Cart, { foreignKey: 'couponId' });
-Cart.belongsTo(Coupon, { foreignKey: 'couponId' });
+db.coupons.hasMany(db.carts, { foreignKey: 'couponId' });
+db.carts.belongsTo(db.coupons, { foreignKey: 'couponId' });
 
 export default db;
