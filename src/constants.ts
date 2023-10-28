cec78e2b321f657c86b6951473e0f6fa02d13b66
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-10-28 20:00:17 +0100

    feat: add constants for file upload operation

--- src/constants.ts
+++ src/constants.ts
@@ -15,3 +15,7 @@ export const CART_STATES = {
     DELIVERED: 'DELIVERED',
     CANCELLED: 'CANCELLED',
 };
+
+export const ONE_MB_IN_BYTES: number = 1000000;
+export const profileImageUploadLimitInMb: number = 5;
+export const ALLOWED_IMAGE_TYPES: string[] = ['jpg', 'jpeg', 'png', 'gif'];
