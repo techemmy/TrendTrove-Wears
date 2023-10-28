6ce29b5577f8efc0e1a28bed61fdf283439470af
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-10-28 19:59:32 +0100

    feat(config): add config for file upload environment variables

---
+++ src/config/cloudinaryConfig.ts
@@ -0,0 +1,5 @@
+export default {
+    CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    API_KEY: process.env.CLOUDINARY_API_KEY,
+    API_SECRET: process.env.CLOUDINARY_API_SECRET,
+};
--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,12 @@ import appConfig from './appConfig';
 import databaseConfig from './databaseConfig';
 import googleOAuthConfig from './googleOAuthConfig';
 import sessionConfig from './sessionConfig';
+import cloudinaryConfig from './cloudinaryConfig';
 
 export {
     databaseConfig as dbConfig,
     appConfig,
     googleOAuthConfig,
     sessionConfig,
+    cloudinaryConfig,
 };
