a46a0bb2e29261906632ddcd3d0eda243019e724
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-11-08 23:52:14 +0100

    feat: update views with products from the database

--- src/routes/adminRoute.ts
+++ src/routes/adminRoute.ts
@@ -1,9 +1,12 @@
 import router, { type Router } from 'express';
+import db from '../database';
 
 const adminRouter: Router = router();
+const Product = db.products;
 
-adminRouter.get('/', (req, res) => {
-    res.render('admin/dashboard');
+adminRouter.get('/', async (req, res) => {
+    const products = await Product.findAll();
+    res.render('admin/dashboard', { products });
 });
 
 export default adminRouter;
