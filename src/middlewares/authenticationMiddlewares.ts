c6c1f52d4f591f11ff7cb29ecb5105d8f2f51c9b
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-11-09 19:22:54 +0100

    feat: create middleware to protect routes for admins only

--- src/middlewares/authenticationMiddlewares.ts
+++ src/middlewares/authenticationMiddlewares.ts
@@ -1,5 +1,7 @@
 import type { NextFunction, Request, Response } from 'express';
 import { setFlashMessage } from '../utilities';
+import type { IRequestWithAuthenticatedUser } from '../types/requestTypes';
+import { USER_ROLES } from '../constants';
 
 export function ensureLoggedInMiddleware(
     req: Request,
@@ -12,6 +14,22 @@ export function ensureLoggedInMiddleware(
             message: 'You need to login to continue',
         });
         res.redirect('/auth/login');
+        return;
+    }
+    next();
+}
+
+export async function ensureAdminUserMiddleware(
+    req: IRequestWithAuthenticatedUser,
+    res: Response,
+    next: NextFunction
+): Promise<void> {
+    if (req.user === undefined || req.user.role !== USER_ROLES.admin) {
+        setFlashMessage(req, {
+            type: 'warning',
+            message: 'You are not authorized to access this feature',
+        });
+        res.redirect('back');
         return;
     }
     next();
