061a4bf6250941c87da9332719fa7b33d41c3c6f
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-11-13 22:09:05 +0100

    feat(route): implement data validation on the createProduct route

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -4,6 +4,8 @@ import multer, { MulterError } from 'multer';
 import { ONE_MB_IN_BYTES, fileUploadLimit } from '../constants';
 import { setFlashMessage } from '../utilities';
 import { ensureAdminUserMiddleware } from '../middlewares/authenticationMiddlewares';
+import { body } from 'express-validator';
+import validationErrorHandlerMiddleware from '../middlewares/validationErrorHandlerMiddleware';
 
 const productRouter: Router = router();
 const getProductImage = multer({
@@ -14,7 +16,6 @@ productRouter.get('/', productController.getAllProduct);
 
 productRouter.post(
     '/',
-    // TODO: add field validation
     (req, res, next) => {
         getProductImage(req, res, (err) => {
             if (err !== undefined && err?.code === 'LIMIT_FILE_SIZE') {
@@ -35,6 +36,37 @@ productRouter.post(
             next();
         });
     },
+    [
+        body('name')
+            .trim()
+            .escape()
+            .notEmpty()
+            .withMessage('Product name is required'),
+        body('price')
+            .trim()
+            .escape()
+            .notEmpty()
+            .withMessage('Product price is required')
+            .toFloat()
+            .isFloat({ min: 0 })
+            .withMessage('Product price cannot be less than zero'),
+        body('category')
+            .trim()
+            .escape()
+            .notEmpty()
+            .withMessage('Product category is required'),
+        body('sizes')
+            .trim()
+            .escape()
+            .notEmpty()
+            .withMessage('Choose at least one size'),
+        body('shortDescription')
+            .trim()
+            .escape()
+            .notEmpty()
+            .withMessage('A short description of the product is required'),
+    ],
+    validationErrorHandlerMiddleware,
     productController.postCreateProduct
 );
 
