580a16f8f3ea8003eddb2f770ea8efe75da07e4c
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-12-14 15:58:01 +0100

    style: add comments to specify different uses cases

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,35 @@
+// --- AOS animation setup -----
 AOS.init({
-	duration: 800,
-	easing: 'slide',
-	once: true
+    duration: 800,
+    easing: 'slide',
+    once: true
 });
 
+
+// ------ handle loading effect on user profile image upload ----------
 const uploadImgBtn = document.querySelector('#uploadProfileImgBtn');
 if (uploadImgBtn) {
-	uploadImgBtn.addEventListener('click', function () {
-		this.innerHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
-	})
+    uploadImgBtn.addEventListener('click', function () {
+        this.innerHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
+    })
 }
 
+// ---------- handle the price range filer on the shop page ---------------
 const rangeInput = document.querySelectorAll(".range-input input"),
-priceInput = document.querySelectorAll(".price-input input"),
-range = document.querySelector(".slider .progress");
+    priceInput = document.querySelectorAll(".price-input input"),
+    range = document.querySelector(".slider .progress");
 let priceGap = 100;
 
-priceInput.forEach(input =>{
-    input.addEventListener("input", e =>{
+priceInput.forEach(input => {
+    input.addEventListener("input", e => {
         let minPrice = parseInt(priceInput[0].value),
-        maxPrice = parseInt(priceInput[1].value);
+            maxPrice = parseInt(priceInput[1].value);
 
-        if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
-            if(e.target.className === "input-min"){
+        if ((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max) {
+            if (e.target.className === "input-min") {
                 rangeInput[0].value = minPrice;
                 range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
-            }else{
+            } else {
                 rangeInput[1].value = maxPrice;
                 range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
             }
@@ -33,18 +37,18 @@ priceInput.forEach(input =>{
     });
 });
 
-rangeInput.forEach(input =>{
-    input.addEventListener("input", e =>{
+rangeInput.forEach(input => {
+    input.addEventListener("input", e => {
         let minVal = parseInt(rangeInput[0].value),
-        maxVal = parseInt(rangeInput[1].value);
+            maxVal = parseInt(rangeInput[1].value);
 
-        if((maxVal - minVal) < priceGap){
-            if(e.target.className === "range-min"){
+        if ((maxVal - minVal) < priceGap) {
+            if (e.target.className === "range-min") {
                 rangeInput[0].value = maxVal - priceGap
-            }else{
+            } else {
                 rangeInput[1].value = minVal + priceGap;
             }
-        }else{
+        } else {
             priceInput[0].value = minVal;
             priceInput[1].value = maxVal;
             range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
