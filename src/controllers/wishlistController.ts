76cf677d789d3b2d550e7c253517ca5d82db4eac
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-01-08 17:50:57 +0100

    feat: implement add/remove to/from wishlist features

--- src/controllers/wishlistController.ts
+++ src/controllers/wishlistController.ts
@@ -2,6 +2,7 @@ import type { Request, Response, NextFunction } from 'express';
 import { IRequestWithAuthenticatedUser } from '../types/requestTypes';
 import db from '../database';
 import { ProductAttributes } from '../types/models/productTypes';
+import { setFlashMessage } from '../utilities';
 
 const User = db.users;
 const Product = db.products;
@@ -11,21 +12,26 @@ export async function getWishlist(req: IRequestWithAuthenticatedUser, res: Respo
         const user = await User.findByPk(req.user.id);
         const productsIds = user?.wishlist ?? [];
         const productsIdsLength = productsIds.length;
-
+        
+        // TODO: refactor code
         const wishlistPromises: Array<[number, Promise<ProductAttributes | null>]> = productsIds.map(productId => [productId, Product.findByPk(productId)]);
         const wishlist: any = [];
 
         for (const promise of wishlistPromises) {
             const product = await promise[1];
             wishlist.push(product);
-            
+                        
             if (product === null) {
                  productsIds.splice(productsIds.indexOf(promise[0]), 1); 
             };
         }
-
+        
         if (productsIdsLength !== productsIds.length) {
-            await user?.update({ wishlist: productsIds }); 
+            await User.update({ wishlist: productsIds }, {
+                where: {
+                    id: req.user.id,
+                },
+            });
         };
         
         res.render('wishlist', { wishlist }); 
@@ -33,5 +39,64 @@ export async function getWishlist(req: IRequestWithAuthenticatedUser, res: Respo
        console.log(err);
        next(err);
     }
+}
+
+export async function getAddProductToWishlist(req: IRequestWithAuthenticatedUser, res: Response, next: NextFunction) {
+    const productId = parseInt(req.params.productId);
+
+    const user = await User.findByPk(req.user.id);
+    
+    let userWishlist = user?.wishlist;
+    if (user === null || userWishlist?.includes(productId)) {
+        res.redirect('back');
+        return;
+    }
+    
+    if (!Array.isArray(userWishlist)) {
+       userWishlist = [productId]; 
+    } else {
+        userWishlist = [...userWishlist, productId];
+    }
+
+    await User.update({ wishlist: userWishlist  }, {
+        where: {
+            id: req.user.id,
+        },
+    });
+    setFlashMessage(req, {
+        type: 'success',
+        message: 'Product added to wishlist!',
+    });
+    res.redirect('back');
+}
+
+export async function getRemoveProductFromWishlist(req: IRequestWithAuthenticatedUser, res: Response, next: NextFunction) {
+    const productId = parseInt(req.params.productId);
+        
+    const user = await User.findByPk(req.user.id);
+    const userWishlist = user?.wishlist;
+    if (user === null || !Array.isArray(userWishlist)) {
+        res.redirect('back');
+        return;
+    };
+
+    const productIdx = userWishlist.indexOf(productId);
+    if (typeof productIdx !== 'number' || productIdx === -1) {
+        res.redirect('back');
+        return;
+    };
+    
+    userWishlist.splice(productIdx, 1);
+    await User.update({ wishlist: userWishlist  }, {
+        where: {
+            id: req.user.id,
+        },
+    });
+    
+    setFlashMessage(req, {
+        type: 'success',
+        message: 'Product removed from wishlist!',
+    });
+    res.redirect('back');
 }
 
--- src/routes/wishlistRoute.ts
+++ src/routes/wishlistRoute.ts
@@ -5,5 +5,9 @@ const wishlistRouter: Router = router();
 
 wishlistRouter.get('/', wishlistController.getWishlist);
 
+wishlistRouter.get('/add/:productId', wishlistController.getAddProductToWishlist);
+
+wishlistRouter.get('/remove/:productId', wishlistController.getRemoveProductFromWishlist);
+
 export default wishlistRouter;
 
